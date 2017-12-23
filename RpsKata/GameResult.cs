0487f1f720054e64707250c1330eade43bf69a5a
Andres Moschini <andresmoschini@gmail.com>
2017-12-23 14:53:18 -0300

    Add system structure

---
+++ RpsKata/GameResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpsKata
+{
+    public enum GameResult
+    {
+        Draw,
+        Player1Won,
+        Player2Won
+    }
+}
---
+++ RpsKata/GameSetup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpsKata
+{
+    public class GameSetup
+    {
+        public GameChoice Player1Choice { get; set; }
+        public GameChoice Player2Choice { get; set; }
+    }
+}
