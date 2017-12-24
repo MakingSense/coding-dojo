579c5650747351bf4eb0b5d68cc8bfa1520ec844
Andres Moschini <andresmoschini@gmail.com>
2017-12-24 10:45:39 -0300

    Add support for Spock and Lizard (RED/GREEN) - Fix ExtensionRulesGameReferee
    **Attention!** something is wrong: All test passed when they should not. See the next commit for more information.

--- RpsKata/ExtensionRulesGameReferee.cs
+++ RpsKata/ExtensionRulesGameReferee.cs
@@ -12,12 +12,28 @@ namespace RpsKata
             (GameChoice.Rock, GameChoice.Rock, GameResult.Draw),
             (GameChoice.Paper, GameChoice.Paper, GameResult.Draw),
             (GameChoice.Scissors, GameChoice.Scissors, GameResult.Draw),
+            (GameChoice.Spock, GameChoice.Spock, GameResult.Draw),
+            (GameChoice.Lizard, GameChoice.Lizard, GameResult.Draw),
             (GameChoice.Rock, GameChoice.Scissors, GameResult.Player1Won),
+            (GameChoice.Rock, GameChoice.Lizard, GameResult.Player1Won),
             (GameChoice.Paper, GameChoice.Rock, GameResult.Player1Won),
+            (GameChoice.Paper, GameChoice.Spock, GameResult.Player1Won),
             (GameChoice.Scissors, GameChoice.Paper, GameResult.Player1Won),
+            (GameChoice.Scissors, GameChoice.Lizard, GameResult.Player1Won),
+            (GameChoice.Spock, GameChoice.Scissors, GameResult.Player1Won),
+            (GameChoice.Spock, GameChoice.Rock, GameResult.Player1Won),
+            (GameChoice.Lizard, GameChoice.Spock, GameResult.Player1Won),
+            (GameChoice.Lizard, GameChoice.Paper, GameResult.Player1Won),
             (GameChoice.Scissors, GameChoice.Rock, GameResult.Player2Won),
+            (GameChoice.Scissors, GameChoice.Spock, GameResult.Player2Won),
             (GameChoice.Rock, GameChoice.Paper, GameResult.Player2Won),
-            (GameChoice.Paper, GameChoice.Scissors, GameResult.Player2Won)
+            (GameChoice.Rock, GameChoice.Spock, GameResult.Player2Won),
+            (GameChoice.Paper, GameChoice.Scissors, GameResult.Player2Won),
+            (GameChoice.Paper, GameChoice.Lizard, GameResult.Player2Won),
+            (GameChoice.Spock, GameChoice.Paper, GameResult.Player2Won),
+            (GameChoice.Spock, GameChoice.Lizard, GameResult.Player2Won),
+            (GameChoice.Lizard, GameChoice.Rock, GameResult.Player2Won),
+            (GameChoice.Lizard, GameChoice.Scissors, GameResult.Player2Won)
         };
 
         public GameResult Evaluate(GameSetup gameSetup) => _rules
