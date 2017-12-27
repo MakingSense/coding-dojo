da2ba9fb6b4ec9178a80151cff6d6d6ea78bcae9
Andres Moschini <andresmoschini@gmail.com>
2017-12-27 16:50:05 -0300

    Add explanation to result (GREEN) - Fix OopAdapterRulesGameReferee

--- Miscellaneous/rock-paper-scissors-no-regex/Solutions/amoschini/RpsKata/OopAdapterRulesGameReferee.cs
+++ Miscellaneous/rock-paper-scissors-no-regex/Solutions/amoschini/RpsKata/OopAdapterRulesGameReferee.cs
@@ -9,38 +9,38 @@ namespace RpsKata
     {
         public GameResult Evaluate(GameSetup gameSetup)
         {
-            var player1Choice = MapEnumToObject(gameSetup.Player1Choice, GameResult.Player1Won(gameSetup.Player1Choice, "TODO", gameSetup.Player2Choice));
-            var player2Choice = MapEnumToObject(gameSetup.Player2Choice, GameResult.Player2Won(gameSetup.Player2Choice, "TODO", gameSetup.Player1Choice));
+            var player1Choice = MapEnumToObject(gameSetup.Player1Choice, GameResult.Player1Won);
+            var player2Choice = MapEnumToObject(gameSetup.Player2Choice, GameResult.Player2Won);
             return player1Choice.PlayAgainst(player2Choice);
         }
 
         // This method is only necessary to adapt this new implementation to previous 
         // interface. If we decided to use this implementation definitivaly, it should
         // not be necessary.
-        private Choice MapEnumToObject(GameChoice choiceEnum, GameResult resultIfItWins)
+        private Choice MapEnumToObject(GameChoice choiceEnum, Func<GameChoice, string, GameChoice, GameResult> resultFactoryIfItWins)
         {
             switch (choiceEnum)
             {
-                case GameChoice.Rock: return new Rock(resultIfItWins);
-                case GameChoice.Paper: return new Paper(resultIfItWins);
-                case GameChoice.Scissors: return new Scissors(resultIfItWins);
-                case GameChoice.Lizard: return new Lizard(resultIfItWins);
-                case GameChoice.Spock: return new Spock(resultIfItWins);
+                case GameChoice.Rock: return new Rock(resultFactoryIfItWins);
+                case GameChoice.Paper: return new Paper(resultFactoryIfItWins);
+                case GameChoice.Scissors: return new Scissors(resultFactoryIfItWins);
+                case GameChoice.Lizard: return new Lizard(resultFactoryIfItWins);
+                case GameChoice.Spock: return new Spock(resultFactoryIfItWins);
                 default: throw new ArgumentOutOfRangeException(nameof(choiceEnum));
             }
         }
 
         private abstract class Choice
         {
-            private readonly GameResult _resultIfIWin;
+            private readonly Func<GameChoice, string, GameChoice, GameResult> _resultFactoryIfItWins;
 
-            public Choice(GameResult resultIfIWin)
+            public Choice(Func<GameChoice, string, GameChoice, GameResult> resultFactoryIfItWins)
             {
-                _resultIfIWin = resultIfIWin;
+                _resultFactoryIfItWins = resultFactoryIfItWins;
             }
 
             protected GameResult Draw() => GameResult.Draw(ToGameChoice());
-            public GameResult IWin() => _resultIfIWin;
+            public GameResult IWin(string verb, Choice looser) => _resultFactoryIfItWins(ToGameChoice(), verb, looser.ToGameChoice());
 
             public abstract GameResult PlayAgainst(Choice other);
             public abstract GameResult PlayAgainst(Rock other);
@@ -54,59 +54,59 @@ namespace RpsKata
 
         private class Rock : Choice
         {
-            public Rock(GameResult resultIfIWin) : base(resultIfIWin) { }
+            public Rock(Func<GameChoice, string, GameChoice, GameResult> resultFactoryIfItWins) : base(resultFactoryIfItWins) { }
             public override GameResult PlayAgainst(Choice other) => other.PlayAgainst(this);
             public override GameResult PlayAgainst(Rock other) => Draw();
             public override GameResult PlayAgainst(Paper other) => other.PlayAgainst(this);
-            public override GameResult PlayAgainst(Scissors other) => IWin();
-            public override GameResult PlayAgainst(Lizard other) => IWin();
+            public override GameResult PlayAgainst(Scissors other) => IWin("crushes", other);
+            public override GameResult PlayAgainst(Lizard other) => IWin("crushes", other);
             public override GameResult PlayAgainst(Spock other) => other.PlayAgainst(this);
             public override GameChoice ToGameChoice() => GameChoice.Rock;
         }
 
         private class Paper : Choice
         {
-            public Paper(GameResult resultIfIWin) : base(resultIfIWin) { }
+            public Paper(Func<GameChoice, string, GameChoice, GameResult> resultFactoryIfItWins) : base(resultFactoryIfItWins) { }
             public override GameResult PlayAgainst(Choice other) => other.PlayAgainst(this);
-            public override GameResult PlayAgainst(Rock other) => IWin();
+            public override GameResult PlayAgainst(Rock other) => IWin("covers", other);
             public override GameResult PlayAgainst(Paper other) => Draw();
             public override GameResult PlayAgainst(Scissors other) => other.PlayAgainst(this);
             public override GameResult PlayAgainst(Lizard other) => other.PlayAgainst(this);
-            public override GameResult PlayAgainst(Spock other) => IWin();
+            public override GameResult PlayAgainst(Spock other) => IWin("disproves", other);
             public override GameChoice ToGameChoice() => GameChoice.Paper;
         }
 
         private class Scissors : Choice
         {
-            public Scissors(GameResult resultIfIWin) : base(resultIfIWin) { }
+            public Scissors(Func<GameChoice, string, GameChoice, GameResult> resultFactoryIfItWins) : base(resultFactoryIfItWins) { }
             public override GameResult PlayAgainst(Choice other) => other.PlayAgainst(this);
             public override GameResult PlayAgainst(Rock other) => other.PlayAgainst(this);
-            public override GameResult PlayAgainst(Paper other) => IWin();
+            public override GameResult PlayAgainst(Paper other) => IWin("cuts", other);
             public override GameResult PlayAgainst(Scissors other) => Draw();
-            public override GameResult PlayAgainst(Lizard other) => IWin();
+            public override GameResult PlayAgainst(Lizard other) => IWin("decapitates", other);
             public override GameResult PlayAgainst(Spock other) => other.PlayAgainst(this);
             public override GameChoice ToGameChoice() => GameChoice.Scissors;
         }
 
         private class Lizard : Choice
         {
-            public Lizard(GameResult resultIfIWin) : base(resultIfIWin) { }
+            public Lizard(Func<GameChoice, string, GameChoice, GameResult> resultFactoryIfItWins) : base(resultFactoryIfItWins) { }
             public override GameResult PlayAgainst(Choice other) => other.PlayAgainst(this);
             public override GameResult PlayAgainst(Rock other) => other.PlayAgainst(this);
-            public override GameResult PlayAgainst(Paper other) => IWin();
+            public override GameResult PlayAgainst(Paper other) => IWin("eats", other);
             public override GameResult PlayAgainst(Scissors other) => other.PlayAgainst(this);
             public override GameResult PlayAgainst(Lizard other) => Draw();
-            public override GameResult PlayAgainst(Spock other) => IWin();
+            public override GameResult PlayAgainst(Spock other) => IWin("poisons", other);
             public override GameChoice ToGameChoice() => GameChoice.Lizard;
         }
 
         private class Spock : Choice
         {
-            public Spock(GameResult resultIfIWin) : base(resultIfIWin) { }
+            public Spock(Func<GameChoice, string, GameChoice, GameResult> resultFactoryIfItWins) : base(resultFactoryIfItWins) { }
             public override GameResult PlayAgainst(Choice other) => other.PlayAgainst(this);
-            public override GameResult PlayAgainst(Rock other) => IWin();
+            public override GameResult PlayAgainst(Rock other) => IWin("vaporizes", other);
             public override GameResult PlayAgainst(Paper other) => other.PlayAgainst(this);
-            public override GameResult PlayAgainst(Scissors other) => IWin();
+            public override GameResult PlayAgainst(Scissors other) => IWin("smashes", other);
             public override GameResult PlayAgainst(Lizard other) => other.PlayAgainst(this);
             public override GameResult PlayAgainst(Spock other) => Draw();
             public override GameChoice ToGameChoice() => GameChoice.Spock;
