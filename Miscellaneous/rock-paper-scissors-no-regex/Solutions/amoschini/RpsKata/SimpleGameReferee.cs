b187a7465a565abbe30e97b2c47750205270daa4
Andres Moschini <andresmoschini@gmail.com>
2017-12-27 15:53:54 -0300

    Add explanation to result (RED) - Fix SimpleGameReferee

--- Miscellaneous/rock-paper-scissors-no-regex/Solutions/amoschini/RpsKata/SimpleGameReferee.cs
+++ Miscellaneous/rock-paper-scissors-no-regex/Solutions/amoschini/RpsKata/SimpleGameReferee.cs
@@ -8,26 +8,24 @@ namespace RpsKata
     {
         public GameResult Evaluate(GameSetup gameSetup)
         {
-            if (gameSetup.Player1Choice == gameSetup.Player2Choice)
-            {
-                return GameResult.Draw(gameSetup.Player1Choice);
-            }
+            return Evaluate(GameResult.Player1Won, gameSetup.Player1Choice, gameSetup.Player2Choice)
+                ?? Evaluate(GameResult.Player2Won, gameSetup.Player2Choice, gameSetup.Player1Choice)
+                ?? GameResult.Draw(gameSetup.Player1Choice);
+        }
 
-            if (gameSetup.Player1Choice == GameChoice.Rock && gameSetup.Player2Choice == GameChoice.Scissors
-                || gameSetup.Player1Choice == GameChoice.Rock && gameSetup.Player2Choice == GameChoice.Lizard
-                || gameSetup.Player1Choice == GameChoice.Paper && gameSetup.Player2Choice == GameChoice.Rock
-                || gameSetup.Player1Choice == GameChoice.Paper && gameSetup.Player2Choice == GameChoice.Spock
-                || gameSetup.Player1Choice == GameChoice.Scissors && gameSetup.Player2Choice == GameChoice.Paper
-                || gameSetup.Player1Choice == GameChoice.Scissors && gameSetup.Player2Choice == GameChoice.Lizard
-                || gameSetup.Player1Choice == GameChoice.Spock && gameSetup.Player2Choice == GameChoice.Scissors
-                || gameSetup.Player1Choice == GameChoice.Spock && gameSetup.Player2Choice == GameChoice.Rock
-                || gameSetup.Player1Choice == GameChoice.Lizard && gameSetup.Player2Choice == GameChoice.Spock
-                || gameSetup.Player1Choice == GameChoice.Lizard && gameSetup.Player2Choice == GameChoice.Paper)
-            {
-                return GameResult.Player1Won(gameSetup.Player1Choice, "TODO", gameSetup.Player2Choice);
-            }
-
-            return GameResult.Player2Won(gameSetup.Player2Choice, "TODO", gameSetup.Player1Choice);
+        private GameResult Evaluate(Func<GameChoice, string, GameChoice, GameResult> playerWon, GameChoice playerAChoice, GameChoice playerBChoice)
+        {
+            return playerAChoice == GameChoice.Rock && playerBChoice == GameChoice.Scissors ? playerWon(playerAChoice, "crushes", playerBChoice)
+                : playerAChoice == GameChoice.Rock && playerBChoice == GameChoice.Lizard ? playerWon(playerAChoice, "crushes", playerBChoice)
+                : playerAChoice == GameChoice.Paper && playerBChoice == GameChoice.Rock ? playerWon(playerAChoice, "covers", playerBChoice)
+                : playerAChoice == GameChoice.Paper && playerBChoice == GameChoice.Spock ? playerWon(playerAChoice, "disproves", playerBChoice)
+                : playerAChoice == GameChoice.Scissors && playerBChoice == GameChoice.Paper ? playerWon(playerAChoice, "cuts", playerBChoice)
+                : playerAChoice == GameChoice.Scissors && playerBChoice == GameChoice.Lizard ? playerWon(playerAChoice, "decapitates", playerBChoice)
+                : playerAChoice == GameChoice.Spock && playerBChoice == GameChoice.Scissors ? playerWon(playerAChoice, "smashes", playerBChoice)
+                : playerAChoice == GameChoice.Spock && playerBChoice == GameChoice.Rock ? playerWon(playerAChoice, "vaporizes", playerBChoice)
+                : playerAChoice == GameChoice.Lizard && playerBChoice == GameChoice.Spock ? playerWon(playerAChoice, "poisons", playerBChoice)
+                : playerAChoice == GameChoice.Lizard && playerBChoice == GameChoice.Paper ? playerWon(playerAChoice, "eats", playerBChoice)
+                : null;
         }
     }
 }
