6aceebbe6be2d6faa4068c3e3da9b01cba5126c6
Andres Moschini <andresmoschini@gmail.com>
2017-12-27 16:00:03 -0300

    Add explanation to result (RED) - Fix WinnerRulesGameReferee

--- Miscellaneous/rock-paper-scissors-no-regex/Solutions/amoschini/RpsKata/WinnerRulesGameReferee.cs
+++ Miscellaneous/rock-paper-scissors-no-regex/Solutions/amoschini/RpsKata/WinnerRulesGameReferee.cs
@@ -7,23 +7,23 @@ namespace RpsKata
 {
     public class WinnerRulesGameReferee : IGameReferee
     {
-        private HashSet<(GameChoice winnerChoice, GameChoice looserChoice)> _rules = new HashSet<(GameChoice winnerChoice, GameChoice looserChoice)>()
+        private Dictionary<(GameChoice winnerChoice, GameChoice looserChoice), string> _verbByRule = new Dictionary<(GameChoice winnerChoice, GameChoice looserChoice), string>()
         {
-            (GameChoice.Rock, GameChoice.Scissors),
-            (GameChoice.Rock, GameChoice.Lizard),
-            (GameChoice.Paper, GameChoice.Rock),
-            (GameChoice.Paper, GameChoice.Spock),
-            (GameChoice.Scissors, GameChoice.Paper),
-            (GameChoice.Scissors, GameChoice.Lizard),
-            (GameChoice.Spock, GameChoice.Scissors),
-            (GameChoice.Spock, GameChoice.Rock),
-            (GameChoice.Lizard, GameChoice.Spock),
-            (GameChoice.Lizard, GameChoice.Paper)
+            [(GameChoice.Rock, GameChoice.Scissors)] = "crushes",
+            [(GameChoice.Rock, GameChoice.Lizard)] = "crushes",
+            [(GameChoice.Paper, GameChoice.Rock)] = "covers",
+            [(GameChoice.Paper, GameChoice.Spock)] = "disproves",
+            [(GameChoice.Scissors, GameChoice.Paper)] = "cuts",
+            [(GameChoice.Scissors, GameChoice.Lizard)] = "decapitates",
+            [(GameChoice.Spock, GameChoice.Scissors)] = "smashes",
+            [(GameChoice.Spock, GameChoice.Rock)] = "vaporizes",
+            [(GameChoice.Lizard, GameChoice.Spock)] = "poisons",
+            [(GameChoice.Lizard, GameChoice.Paper)] = "eats",
         };
 
         public GameResult Evaluate(GameSetup gameSetup) =>
-            _rules.Contains((gameSetup.Player1Choice, gameSetup.Player2Choice)) ? GameResult.Player1Won(gameSetup.Player1Choice, "TODO", gameSetup.Player2Choice)
-            : _rules.Contains((gameSetup.Player2Choice, gameSetup.Player1Choice)) ? GameResult.Player2Won(gameSetup.Player2Choice, "TODO", gameSetup.Player1Choice)
+            _verbByRule.TryGetValue((gameSetup.Player1Choice, gameSetup.Player2Choice), out var verb1) ? GameResult.Player1Won(gameSetup.Player1Choice, verb1, gameSetup.Player2Choice)
+            : _verbByRule.TryGetValue((gameSetup.Player2Choice, gameSetup.Player1Choice), out var verb2) ? GameResult.Player2Won(gameSetup.Player2Choice, verb2, gameSetup.Player1Choice)
             : GameResult.Draw(gameSetup.Player1Choice);
     }
 }
